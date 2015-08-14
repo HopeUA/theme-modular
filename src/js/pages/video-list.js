866b85361fe14cc3dc559be8662d25cf7c86a192
neomulik <neomulik@hotmail.com>
2015-08-14 13:46:45 +0300

    work with js loadMore

---
+++ src/js/pages/video-list.js
@@ -0,0 +1,93 @@
+$(function () {
+
+    var place = $('.content-video-list-items');
+    var url = 'ajax/videoList0.json';
+
+    $.getJSON(url, function (episodes) {
+        $.each(episodes, function (index, element) {
+
+            var labels = '';
+
+            $.each(element.episodeLabels, function (index, element) {
+                var label = '<a href="#" class="content-video-list-content-label">' + element + '</a>';
+                labels += label;
+            });
+
+            var template = '<div class="content-video-list-item">' +
+                '<div class="container">' +
+                    '<div class="container-content">' +
+                        '<div class="content-video-list-poster">' +
+                            '<div class="content-video-list-poster-play"></div>' +
+                            '<img src="img/' + element.episodeImg + '" alt="">' +
+                        '</div>' +
+                        '<div class="content-video-list-content">' +
+                            '<p class="content-video-list-content-title">' + element.episodeTitle + '</p>' +
+                            '<p class="content-video-list-content-date">' + timeToStr2(element.episodeDate, 'ru') + '</p>' +
+                            '<div class="content-video-list-content-labels">' + labels + '</div>' +
+                            '<p class="content-video-list-content-description">' + element.episodeDescription + '</p>' +
+                        '</div>' +
+                    '</div>' +
+                '</div>' +
+                '</div>';
+
+            place.append(template);
+        });
+    });
+
+    function timeToStr2(unixTime, lang) {
+        moment.locale(lang);
+        var strDate = moment.unix(unixTime).format('D MMMM') + '<span>, ' + moment.unix(unixTime).format('YYYY') + '</span>';
+        return strDate;
+    }
+
+
+
+    setTimeout(function() {
+
+        var counter = 1;
+
+        $(window).scroll(function () {
+
+            var scrollHeight = $(document).height() - $(window).height();
+
+            if ((scrollHeight - $(window).scrollTop()) <= 1851) {
+
+                var place = $('.content-video-list-items');
+                var url = 'ajax/videoList' + counter +'.json';
+
+                $.getJSON(url, function (episodes) {
+                    $.each(episodes, function (index, element) {
+
+                        var labels = '';
+
+                        $.each(element.episodeLabels, function (index, element) {
+                            var label = '<a href="#" class="content-video-list-content-label">' + element + '</a>';
+                            labels += label;
+                        });
+
+                        var template = '<div class="content-video-list-item">' +
+                            '<div class="container">' +
+                            '<div class="container-content">' +
+                            '<div class="content-video-list-poster">' +
+                            '<div class="content-video-list-poster-play"></div>' +
+                            '<img src="img/' + element.episodeImg + '" alt="">' +
+                            '</div>' +
+                            '<div class="content-video-list-content">' +
+                            '<p class="content-video-list-content-title">' + element.episodeTitle + '</p>' +
+                            '<p class="content-video-list-content-date">' + timeToStr2(element.episodeDate, 'ru') + '</p>' +
+                            '<div class="content-video-list-content-labels">' + labels + '</div>' +
+                            '<p class="content-video-list-content-description">' + element.episodeDescription + '</p>' +
+                            '</div>' +
+                            '</div>' +
+                            '</div>' +
+                            '</div>';
+
+                        place.append(template);
+                    });
+                });
+
+                counter++;
+            }
+        });
+    }, 50);
+});
