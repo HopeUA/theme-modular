53027dfae454dd1fe105a0360a3de4a1cbc52e32
Sergey Sadovoi <serg.sadovoi@gmail.com>
2015-08-20 21:16:56 +0300

    [workflow] Add bower for vendor JS

--- src/js/config.js
+++ src/js/config.js
@@ -1,7 +1,12 @@
-var hopeConfig = {
-    api: {
-        media: {
-            endpoint: 'http://tv.hope.ua/api'
+/**
+ * Global namespace for Hope Modules adn Libraries
+ */
+var Hope = {};
+
+Hope.Config = {
+    Api: {
+        Media: {
+            Endpoint: 'http://tv.hope.ua/api'
         }
     }
 };
